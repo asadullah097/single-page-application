6b1526744747f4192ecae358e79b1fa1cb386447
Asad ullah <asaddir5566@gmail.com>
2020-09-17 11:53:03 +0500

    migration

---
+++ app/Models/User.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use App\PlayerDetail;
+use App\Interview;
+use App\Biography;
+use App\CompetenceProfile;
+use App\Club;
+use App\Match;
+use App\Training;
+use App\Video;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+   
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+   
+}
